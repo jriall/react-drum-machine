4bc8775ecbafe640806ed47fe0ddb47bc553ca11
JamesRiall <jamesrariall@gmail.com>
2017-10-11 07:01:34 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+          <h1 className="App-title">Welcome to React</h1>
+    );
+  }
+}
+
+export default App;

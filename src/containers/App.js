45302a618e68647785d01aeaadf6ba9efe2e65ea
JamesRiall <jamesrariall@gmail.com>
2017-10-14 12:53:58 +0100

    rearranged imports

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,25 +1,7 @@
 import React, { Component } from "react";
 import SoundButtonsContainer from "./SoundButtonsContainer";
 import Controls from "./Controls";
-import Heater1 from "../assets/sounds/heater-kit/Heater1.mp3";
-import Heater2 from "../assets/sounds/heater-kit/Heater2.mp3";
-import Heater3 from "../assets/sounds/heater-kit/Heater3.mp3";
-import Heater4 from "../assets/sounds/heater-kit/Heater4.mp3";
-import Heater5 from "../assets/sounds/heater-kit/Heater5.mp3";
-import Heater6 from "../assets/sounds/heater-kit/Heater6.mp3";
-import Heater7 from "../assets/sounds/heater-kit/Heater7.mp3";
-import Heater8 from "../assets/sounds/heater-kit/Heater8.mp3";
-import Heater9 from "../assets/sounds/heater-kit/Heater9.mp3";
-import Smooth1 from "../assets/sounds/smooth-piano-kit/Smooth1.mp3";
-import Smooth2 from "../assets/sounds/smooth-piano-kit/Smooth2.mp3";
-import Smooth3 from "../assets/sounds/smooth-piano-kit/Smooth3.mp3";
-import Smooth4 from "../assets/sounds/smooth-piano-kit/Smooth4.mp3";
-import Smooth5 from "../assets/sounds/smooth-piano-kit/Smooth5.mp3";
-import Smooth6 from "../assets/sounds/smooth-piano-kit/Smooth6.mp3";
-import Smooth7 from "../assets/sounds/smooth-piano-kit/Smooth7.mp3";
-import Smooth8 from "../assets/sounds/smooth-piano-kit/Smooth8.mp3";
-import Smooth9 from "../assets/sounds/smooth-piano-kit/Smooth9.mp3";
-
+import SoundFile from "./sound-file";
 
 class App extends Component {
   constructor(props) {
@@ -35,55 +17,55 @@ class App extends Component {
             name: "Heater-1",
             keyCode: 81,
             keyChar: "Q",
-            src: Heater1
+            src: SoundFile.Heater1
           },
           {
             name: "Heater-2",
             keyCode: 87,
             keyChar: "W",
-            src: Heater2
+            src: SoundFile.Heater2
           },
           {
             name: "Heater-3",
             keyCode: 69,
             keyChar: "E",
-            src: Heater3
+            src: SoundFile.Heater3
           },
           {
             name: "Heater-4",
             keyCode: 65,
             keyChar: "A",
-            src: Heater4
+            src: SoundFile.Heater4
           },
           {
             name: "Clap",
             keyCode: 83,
             keyChar: "S",
-            src: Heater5
+            src: SoundFile.Heater5
           },
           {
             name: "Open-HH",
             keyCode: 68,
             keyChar: "D",
-            src: Heater6
+            src: SoundFile.Heater6
           },
           {
             name: "Kick-n'-Hat",
             keyCode: 90,
             keyChar: "Z",
-            src: Heater7
+            src: SoundFile.Heater7
           },
           {
             name: "Kick",
             keyCode: 88,
             keyChar: "X",
-            src: Heater8
+            src: SoundFile.Heater8
           },
           {
             name: "Closed-HH",
             keyCode: 67,
             keyChar: "C",
-            src: Heater9
+            src: SoundFile.Heater9
           }
         ],
         smoothPianoKit: [
@@ -91,55 +73,55 @@ class App extends Component {
             name: "Chord_1",
             keyCode: 81,
             keyChar: "Q",
-            src: Smooth1
+            src: SoundFile.Smooth1
           },
           {
             name: "Chord_2",
             keyCode: 87,
             keyChar: "W",
-            src: Smooth2
+            src: SoundFile.Smooth2
           },
           {
             name: "Chord_3",
             keyCode: 69,
             keyChar: "E",
-            src: Smooth3
+            src: SoundFile.Smooth3
           },
           {
             name: "Shaker",
             keyCode: 65,
             keyChar: "A",
-            src: Smooth4
+            src: SoundFile.Smooth4
           },
           {
             name: "Open-HH",
             keyCode: 83,
             keyChar: "S",
-            src: Smooth5
+            src: SoundFile.Smooth5
           },
           {
             name: "Closed-HH",
             keyCode: 68,
             keyChar: "D",
-            src: Smooth6
+            src: SoundFile.Smooth6
           },
           {
             name: "Punchy-Kick",
             keyCode: 90,
             keyChar: "Z",
-            src: Smooth7
+            src: SoundFile.Smooth7
           },
           {
             name: "Side-Stick",
             keyCode: 88,
             keyChar: "X",
-            src: Smooth8
+            src: SoundFile.Smooth8
           },
           {
             name: "Snare",
             keyCode: 67,
             keyChar: "C",
-            src: Smooth9
+            src: SoundFile.Smooth9
           }
         ]
       }
@@ -154,6 +136,7 @@ class App extends Component {
 
   //turns power on or off
   togglePower() {
+    console.log(this.state.soundSet)
     this.setState({
       powerOn: !this.state.powerOn,
       display: ""
---
+++ src/containers/sound-file.js
@@ -0,0 +1,39 @@
+import Heater1 from "../assets/sounds/heater-kit/Heater1.mp3";
+import Heater2 from "../assets/sounds/heater-kit/Heater2.mp3";
+import Heater3 from "../assets/sounds/heater-kit/Heater3.mp3";
+import Heater4 from "../assets/sounds/heater-kit/Heater4.mp3";
+import Heater5 from "../assets/sounds/heater-kit/Heater5.mp3";
+import Heater6 from "../assets/sounds/heater-kit/Heater6.mp3";
+import Heater7 from "../assets/sounds/heater-kit/Heater7.mp3";
+import Heater8 from "../assets/sounds/heater-kit/Heater8.mp3";
+import Heater9 from "../assets/sounds/heater-kit/Heater9.mp3";
+import Smooth1 from "../assets/sounds/smooth-piano-kit/Smooth1.mp3";
+import Smooth2 from "../assets/sounds/smooth-piano-kit/Smooth2.mp3";
+import Smooth3 from "../assets/sounds/smooth-piano-kit/Smooth3.mp3";
+import Smooth4 from "../assets/sounds/smooth-piano-kit/Smooth4.mp3";
+import Smooth5 from "../assets/sounds/smooth-piano-kit/Smooth5.mp3";
+import Smooth6 from "../assets/sounds/smooth-piano-kit/Smooth6.mp3";
+import Smooth7 from "../assets/sounds/smooth-piano-kit/Smooth7.mp3";
+import Smooth8 from "../assets/sounds/smooth-piano-kit/Smooth8.mp3";
+import Smooth9 from "../assets/sounds/smooth-piano-kit/Smooth9.mp3";
+
+export default {
+  Heater1,
+  Heater2,
+  Heater3,
+  Heater4,
+  Heater5,
+  Heater6,
+  Heater7,
+  Heater8,
+  Heater9,
+  Smooth1,
+  Smooth2,
+  Smooth3,
+  Smooth4,
+  Smooth5,
+  Smooth6,
+  Smooth7,
+  Smooth8,
+  Smooth9,
+}
